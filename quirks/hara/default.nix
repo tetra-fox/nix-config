99fa9537d396ba8333d19098af75e6556a488f1e
tetra <me@tetra.cool>
2026-04-10 23:19:07 -0700

    feat(quirks): add sleep handler for RAM leds

--- quirks/hara/default.nix
+++ quirks/hara/default.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
-    # ./suspend-hook.nix
+    ./ram-led-suspend-hook.nix
     # ./scarlett-volume-lock.nix
     ./windows-systemd-boot-entry.nix
   ];
---
+++ quirks/hara/ram-led-suspend-hook.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  systemd.services.ram-led-suspend-hook = {
+    description = "Manage RAM LEDs during suspend and resume";
+
+    before = [ "sleep.target" ];
+    wantedBy = [ "sleep.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+
+      # sleep
+      ExecStart = "${pkgs.openrgb}/bin/openrgb --device 'ENE DRAM' --mode Off";
+
+      # wake
+      ExecStop = "${pkgs.openrgb}/bin/openrgb --device 'ENE DRAM' --mode Rainbow --speed 0";
+    };
+  };
+}
