012c64bb25ad365c2b71ef2c89057e24709380d9
tetra <me@tetra.cool>
2026-04-14 16:47:30 -0700

    refactor(kitty): remove unnecessary environment variables from home.nix

--- features/kitty/home.nix
+++ features/kitty/home.nix
@@ -12,11 +12,6 @@
     enableGitIntegration = true;
     shellIntegration.enableZshIntegration = true;
 
-    # See https://sw.kovidgoyal.net/kitty/faq/#things-behave-differently-when-running-kitty-from-system-launcher-vs-from-another-terminal
-    environment = {
-      "read_from_shell" = "PATH LANG LC_* XDG_* EDITOR VISUAL";
-    };
-
     font = {
       name = lib.head config.fonts.fontconfig.defaultFonts.monospace;
       size = 12;
