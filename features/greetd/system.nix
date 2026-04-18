6ecbfc249f946e638e3d7824139810926bb50e66
tetra <me@tetra.cool>
2026-04-18 11:11:27 -0700

    fix(greetd): point tuigreet to current wayland-sessions

--- features/greetd/system.nix
+++ features/greetd/system.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time-format '%A, %B %e - %T' --remember --remember-session -g 'hey kiddo!' --greet-align left";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time-format '%A, %B %e - %T' --remember --remember-session -g 'hey kiddo!' --greet-align left --sessions /run/current-system/sw/share/wayland-sessions";
         user = "greeter";
       };
     };
