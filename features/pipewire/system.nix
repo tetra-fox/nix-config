c27b82cb314c5c725254b91e91b8f0177be7fdd4
tetra <me@tetra.cool>
2026-04-22 21:39:19 -0700

    feat(pipewire): add pulse, jack, shims for pipewire, rtkit

--- features/pipewire/system.nix
+++ features/pipewire/system.nix
@@ -1,9 +1,22 @@
-{ ... }:
+{ username, ... }:
 
 {
   services.pipewire = {
     enable = true;
     audio.enable = true; # use pipewire as primary sound server
     wireplumber.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
   };
+
+  security.rtkit.enable = true;
+
+  users.users.${username}.extraGroups = [
+    "audio"
+    "realtime"
+  ];
 }
