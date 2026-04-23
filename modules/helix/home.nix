e3299e34c365747678368981e72283b479ad6907
tetra <me@tetra.cool>
2026-04-23 00:50:34 -0700

    feat(helix): add helix config

--- modules/helix/home.nix
+++ modules/helix/home.nix
@@ -1,6 +1,23 @@
 {...}: {
   programs.helix = {
     enable = true;
+
     defaultEditor = true;
+
+    settings = {
+      editor = {
+        bufferline = "always";
+        line-number = "relative";
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+      };
+      keys.normal = {
+        tab = ":bn";
+        S-tab = ":bp";
+      };
+    };
   };
 }
