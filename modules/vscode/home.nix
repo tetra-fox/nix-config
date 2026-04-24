3e687758e163e3013752518998d902c6e1a1931c
tetra <me@tetra.cool>
2026-04-23 20:37:56 -0700

    refactor(vscode): clean up vscode config

--- modules/vscode/home.nix
+++ modules/vscode/home.nix
@@ -2,119 +2,91 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  alejandra = inputs.alejandra.packages.${pkgs.stdenv.hostPlatform.system}.default;
+in {
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
     mutableExtensionsDir = false;
     profiles.default = {
-      extensions = with pkgs.open-vsx;
-        [
+      # add `++ (with pkgs.vscode-marketplace; [...])` for extensions not on openvsx
+      extensions = with pkgs.open-vsx; [
           # looks
           vscode-icons-team.vscode-icons
 
-          # language support - nix
-          jnoortheen.nix-ide
+          # tooling
+          albert.tabout
+          anthropic.claude-code
+          bradlc.vscode-tailwindcss
+          esbenp.prettier-vscode
+          jeanp413.open-remote-ssh
 
-          # language support - qt (i know its not a language)
-          theqtcompany.qt-core
-          theqtcompany.qt-qml
-          delgan.qml-format
+          # json5
+          blueglassblock.better-json5
 
-          # language support - rust
-          rust-lang.rust-analyzer
-          tamasfe.even-better-toml
-
-          # language support - lua
+          # lua
           sumneko.lua
 
-          # language support - nodejs
-          dbaeumer.vscode-eslint
-          yoavbls.pretty-ts-errors
-          denoland.vscode-deno
-
-          # language support - svelte
-          svelte.svelte-vscode
-
-          # language support - sieve
-          adzero.vscode-sievehighlight
-
-          # language support - markdown
+          # markdown
           davidanson.vscode-markdownlint
 
-          # language support - shell
+          # nix
+          jnoortheen.nix-ide
+
+          # nodejs / typescript
+          dbaeumer.vscode-eslint
+          denoland.vscode-deno
+          yoavbls.pretty-ts-errors
+
+          # qt
+          delgan.qml-format
+          theqtcompany.qt-core
+          theqtcompany.qt-qml
+
+          # rust
+          rust-lang.rust-analyzer
+
+          # shell
           foxundermoon.shell-format
 
-          # tooling
-          anthropic.claude-code
-          esbenp.prettier-vscode
-          albert.tabout
-          bradlc.vscode-tailwindcss
-          jeanp413.open-remote-ssh
-        ]
-        ++ (with pkgs.vscode-marketplace; [
-          # in case we need extensions NOT available on openvsx
-        ]);
+          # sieve
+          adzero.vscode-sievehighlight
+
+          # svelte
+          svelte.svelte-vscode
+
+          # toml
+          tamasfe.even-better-toml
+        ];
+
       userSettings = {
+        # workbench
         "workbench.iconTheme" = "vscode-icons";
         "workbench.editorAssociations" = {
           "{git,gitlens,chat-editing-snapshot-text-model,copilot,git-graph,git-graph-3}:/**/*.qrc" = "default";
           "*.qrc" = "qt-core.qrcEditor";
         };
 
+        # editor
         "editor.fontLigatures" = true;
         "editor.formatOnSave" = true;
 
+        # terminal
         "terminal.integrated.fontLigatures.enabled" = true;
 
-        "claudeCode.preferredLocation" = "sidebar";
-        "claudeCode.claudeProcessWrapper" = "${pkgs.claude-code}/bin/claude";
-
-        # nix
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
-        "nix.formatterPath" = "${
-          inputs.alejandra.packages.${pkgs.stdenv.hostPlatform.system}.default
-        }/bin/alejandra";
-        "nix.serverSettings" = {
-          nixd = {
-            formatting.command = [
-              "${inputs.alejandra.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/alejandra"
-            ];
-          };
-        };
-        "[nix]" = {
-          "editor.defaultFormatter" = "jnoortheen.nix-ide";
-        };
-
+        # git
         "git.autofetch" = true;
         "git.confirmSync" = false;
         "git.enableSmartCommit" = true;
 
-        # language specific
+        # claude-code
+        "claudeCode.preferredLocation" = "sidebar";
+        "claudeCode.claudeProcessWrapper" = "${pkgs.claude-code}/bin/claude";
+
+        # json
         "[json]" = {
           "editor.defaultFormatter" = "esbenp.prettier-vscode";
-        };
-
-        # qml
-        "[qml]" = {
-          "editor.defaultFormatter" = "delgan.qml-format";
-        };
-        "qt-qml.qmlls.useQmlImportPathEnvVar" = true;
-        "qt-qml.qmlls.customExePath" = "${pkgs.kdePackages.qtdeclarative}/bin/qmlls";
-        "qmlFormat.command" = "${pkgs.kdePackages.qtdeclarative}/bin/qmlformat";
-        "qt-qml.doNotAskForQmllsDownload" = true;
-
-        # shell
-        "shellformat.path" = "${pkgs.shfmt}/bin/shfmt";
-        "[shellscript]" = {
-          "editor.defaultFormatter" = "foxundermoon.shell-format";
-        };
-
-        # rust
-        "rust-analyzer.server.path" = "${pkgs.rust-analyzer}/bin/rust-analyzer";
-        "[rust]" = {
-          "editor.defaultFormatter" = "rust-lang.rust-analyzer";
         };
 
         # lua
@@ -123,18 +95,7 @@
           "editor.defaultFormatter" = "sumneko.lua";
         };
 
-        # svelte
-        "[svelte]" = {
-          "editor.defaultFormatter" = "svelte.svelte-vscode";
-        };
-
-        # deno
-        "deno.path" = "${pkgs.deno}/bin/deno";
-
-        # toml
-        "evenBetterToml.taplo.bundled" = false;
-        "evenBetterToml.taplo.path" = "${pkgs.taplo}/bin/taplo";
-
+        # markdown
         "[markdown]" = {
           "editor.wordWrap" = "on";
           "editor.quickSuggestions" = {
@@ -148,19 +109,63 @@
           };
         };
 
-        "[yaml]" = {
-          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        # nix
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
+        # "nix.formatterPath" = "${alejandra}/bin/alejandra"; since nixd.formatting.command is set, this is ignored
+        "nix.serverSettings" = {
+          nixd = {
+            formatting.command = ["${alejandra}/bin/alejandra"];
+          };
+        };
+        "[nix]" = {
+          "editor.defaultFormatter" = "jnoortheen.nix-ide";
         };
 
+        # qml
+        "qt-qml.qmlls.useQmlImportPathEnvVar" = true;
+        "qt-qml.qmlls.customExePath" = "${pkgs.kdePackages.qtdeclarative}/bin/qmlls";
+        "qt-qml.doNotAskForQmllsDownload" = true;
+        "qmlFormat.command" = "${pkgs.kdePackages.qtdeclarative}/bin/qmlformat";
+        "[qml]" = {
+          "editor.defaultFormatter" = "delgan.qml-format";
+        };
+
+        # rust
+        "rust-analyzer.server.path" = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+        "[rust]" = {
+          "editor.defaultFormatter" = "rust-lang.rust-analyzer";
+        };
+
+        # shell
+        "shellformat.path" = "${pkgs.shfmt}/bin/shfmt";
+        "[shellscript]" = {
+          "editor.defaultFormatter" = "foxundermoon.shell-format";
+        };
+
+        # svelte
+        "[svelte]" = {
+          "editor.defaultFormatter" = "svelte.svelte-vscode";
+        };
+
+        # toml
+        "evenBetterToml.taplo.bundled" = false;
+        "evenBetterToml.taplo.path" = "${pkgs.taplo}/bin/taplo";
         "[toml]" = {
           "editor.defaultFormatter" = "tamasfe.even-better-toml";
         };
 
+        # typescript / javascript / deno
+        "deno.path" = "${pkgs.deno}/bin/deno";
         "[javascript]" = {
           "editor.defaultFormatter" = "esbenp.prettier-vscode";
         };
-
         "[typescript]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+
+        # yaml
+        "[yaml]" = {
           "editor.defaultFormatter" = "esbenp.prettier-vscode";
         };
       };
