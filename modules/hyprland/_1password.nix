f3d8f94b9f49df75e48b12ab87f736305fbe791d
tetra <me@tetra.cool>
2026-06-12 15:50:55 -0700

    fix(hyprland): set 1password QA windowrule

--- modules/hyprland/_1password.nix
+++ modules/hyprland/_1password.nix
@@ -2,8 +2,10 @@
   wayland.windowManager.hyprland.settings = {
     bind = [
       "L_Control&L_Shift,SPACE,exec,1password --quick-access"
-      "L_Control,BACKSLASH,exec,1password --fill"
+      # "L_Control,BACKSLASH,exec,1password --fill" this may work someday.....
     ];
+
+    windowrule = ["match:title Quick Access — 1Password, stay_focused on"];
 
     exec-once = ["app2unit -- 1password --silent"];
   };
